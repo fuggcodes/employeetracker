8a0dc3dbe9b78a4c9805da560ae0a8206f131f64
David Binyatov <davidbphotography101@gmail.com>
2022-04-07 00:05:45 -0400

    function to show all employees

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ showDepartments = () => {
     });
 };
 
-// function to show all roles 
+// function for roles 
 showRoles = () => {
     console.log('Showing all roles...\n');
 
@@ -141,3 +141,25 @@ showRoles = () => {
         promptUser();
     })
 };
+
+// function for employees 
+showEmployees = () => {
+    console.log('Showing all employees...\n'); 
+    const sql = `SELECT employee.id, 
+                        employee.first_name, 
+                        employee.last_name, 
+                        role.title, 
+                        department.name AS department,
+                        role.salary, 
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                 FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+  
+    connection.query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows);
+      promptUser();
+    });
+  };
