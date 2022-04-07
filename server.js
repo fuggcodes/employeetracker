c999f3e890a89ee7a1058be2dbcd88fa582a1184
David Binyatov <davidbphotography101@gmail.com>
2022-04-06 23:54:29 -0400

    inquirer prompt for first action

--- server.js
+++ server.js
@@ -30,3 +30,27 @@ afterConnection = () => {
     console.log("***********************************")
     promptUser();
   };
+
+  // first action inquirer prompt
+const promptUser = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role',
+                  'Update an employee manager',
+                  "View employees by department",
+                  'Delete a department',
+                  'Delete a role',
+                  'Delete an employee',
+                  'View department budgets',
+                  'No Action']
+      }
+    ])
