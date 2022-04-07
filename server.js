7319246aa857466d6e0a0fffe6ce05becf393f37
David Binyatov <davidbphotography101@gmail.com>
2022-04-06 23:57:36 -0400

    add if statements

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 // install mysql2
 const mysql = require('mysql2')
 // install inquirer 
-const inquirer = require('inquirer'); 
+const inquirer = require('inquirer');
 // install console.table
-const cTable = require('console.table'); 
+const cTable = require('console.table');
 
 // connect to database
 const connection = mysql.createConnection({
@@ -13,13 +13,13 @@ const connection = mysql.createConnection({
     user: 'root',
     password: process.env.MYSQL_PASSWORD,
     database: 'employee_db'
-  });
-  
-  connection.connect(err => {
+});
+
+connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     afterConnection();
-  });
+});
 
 // function after connected and initial image shows 
 afterConnection = () => {
@@ -29,28 +29,88 @@ afterConnection = () => {
     console.log("*                                 *")
     console.log("***********************************")
     promptUser();
-  };
+};
 
-  // first action inquirer prompt
+// first action inquirer prompt
 const promptUser = () => {
-    inquirer.prompt ([
-      {
-        type: 'list',
-        name: 'choices', 
-        message: 'What would you like to do?',
-        choices: ['View all departments', 
-                  'View all roles', 
-                  'View all employees', 
-                  'Add a department', 
-                  'Add a role', 
-                  'Add an employee', 
-                  'Update an employee role',
-                  'Update an employee manager',
-                  "View employees by department",
-                  'Delete a department',
-                  'Delete a role',
-                  'Delete an employee',
-                  'View department budgets',
-                  'No Action']
-      }
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Update an employee manager',
+                "View employees by department",
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View department budgets',
+                'No Action']
+        }
     ])
+        .then((answers) => {
+            const { choices } = answers;
+
+            if (choices === "View all departments") {
+                showDepartments();
+            }
+
+            if (choices === "View all roles") {
+                showRoles();
+            }
+
+            if (choices === "View all employees") {
+                showEmployees();
+            }
+
+            if (choices === "Add a department") {
+                addDepartment();
+            }
+
+            if (choices === "Add a role") {
+                addRole();
+            }
+
+            if (choices === "Add an employee") {
+                addEmployee();
+            }
+
+            if (choices === "Update an employee role") {
+                updateEmployee();
+            }
+
+            if (choices === "Update an employee manager") {
+                updateManager();
+            }
+
+            if (choices === "View employees by department") {
+                employeeDepartment();
+            }
+
+            if (choices === "Delete a department") {
+                deleteDepartment();
+            }
+
+            if (choices === "Delete a role") {
+                deleteRole();
+            }
+
+            if (choices === "Delete an employee") {
+                deleteEmployee();
+            }
+
+            if (choices === "View department budgets") {
+                viewBudget();
+            }
+
+            if (choices === "No Action") {
+                connection.end()
+            };
+        });
+};
