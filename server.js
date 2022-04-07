9f5665fe07d7f14ac026914021102b8b056e184c
David Binyatov <davidbphotography101@gmail.com>
2022-04-06 23:48:33 -0400

    connect to database

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const mysql = require('mysql2')
 const inquirer = require('inquirer'); 
 // install console.table
 const cTable = require('console.table'); 
+
+// connect to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employee_db'
+  });
+  
+  connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+  });
