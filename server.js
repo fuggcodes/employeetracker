7601ba23b174607c173d03c8598aeb467e2612d7
David Binyatov <davidbphotography101@gmail.com>
2022-04-06 23:14:27 -0400

    install mysql12, inquirer, console.table

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 require('dotenv').config()
+
+// install mysql2
+const mysql = require('mysql2')
+// install inquirer 
+const inquirer = require('inquirer'); 
+// install console.table
+const cTable = require('console.table'); 
