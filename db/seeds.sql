a80fc3419d0eb2715cf3dd6a3c7e96df27408f1c
David Binyatov <davidbphotography101@gmail.com>
2022-04-08 02:54:24 -0400

    minorities

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,13 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
 ('Photographer', 80000, 1),
-('Videographer', 120000, 1),
-('Editor', 10000, 2), 
+('Videographer', 100000, 1),
+('Editor', 80000, 4), 
 ('Creative Director', 150000, 2),
-('Casting Director', 70000, 3), 
-('Producion Assistant', 90000, 3),
-('Animator', 100000, 4),
-('Project Manager', 90000, 4);
+('Casting Director', 60000, 2), 
+('Producion Assistant', 60000, 3),
+('Animator', 90000, 4),
+('Project Manager', 80000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
